edbaa8cc6fc64bc5b598be69fac6cd1b23ea4be3
Darya Chernikhova <daryachernikhova@nypl.org>
2016-11-20 13:05:34 -0500

    adding heroku database call

---
+++ scripts.py
@@ -0,0 +1,93 @@
+import json
+import logging 
+import os
+
+from model import (
+    Collection, 
+    Configuration, 
+    production_session,
+)
+
+
+class Script(object):
+
+    @property
+    def _db(self):
+        if not hasattr(self, "_session"):
+            self._session = production_session()
+        return self._session
+
+    @property
+    def log(self):
+        if not hasattr(self, '_log'):
+            logger_name = getattr(self, 'name', None)
+            self._log = logging.getLogger(logger_name)
+        return self._log        
+
+    @property
+    def data_directory(self):
+        return Configuration.data_directory()
+
+    @classmethod
+    def parse_command_line(cls, _db=None, cmd_args=None):
+        parser = cls.arg_parser()
+        return parser.parse_known_args(cmd_args)[0]
+
+    @classmethod
+    def arg_parser(cls):
+        raise NotImplementedError()
+
+    @classmethod
+    def parse_time(cls, time_string):
+        """Try to pass the given string as a time."""
+        if not time_string:
+            return None
+        for format in ('%Y-%m-%d', '%m/%d/%Y', '%Y%m%d'):
+            for hours in ('', ' %H:%M:%S'):
+                full_format = format + hours
+                try:
+                    parsed = datetime.datetime.strptime(
+                        time_string, full_format
+                    )
+                    return parsed
+                except ValueError, e:
+                    continue
+        raise ValueError("Could not parse time: %s" % time_string)
+
+    def __init__(self, _db=None):
+        """Basic constructor.
+
+        :_db: A database session to be used instead of
+        creating a new one. Useful in tests.
+        """
+        if _db:
+            self._session = _db
+
+    def run(self):
+        self.load_configuration()
+        try:
+            self.preload_tables(self._db)
+        except Exception, e:
+            logging.error(
+                "Fatal exception while running script: %s", e,
+                exc_info=e
+            )
+            raise e
+
+    def load_configuration(self):
+        if not Configuration.instance:
+            Configuration.load()
+
+
+    def preload_tables(self, db):
+        base_path = os.path.split(__file__)[0]
+        #self.resource_path = os.path.join(base_path, "files", "oneclick")
+        path = os.path.join(base_path, "example.json")
+        data = open(path).read()
+        dict_data = json.loads(data)
+
+
+        print "hi"
+
+
+
